534d3e8a1aed8b856cae66669070db6f0b9df43f
Estela Xu <estelaxu331@gmail.com>
2020-12-16 01:52:57 -0800

    1

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    //Call animator
+    private Animator doorAnimation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Call component 
+        doorAnimation = this.transform.parent.GetComponent<Animator>();
+
+    }
+
+    //Trigger
+
+    void OnTriggerEnter(Collider other)
+    {
+        //When first person controller trigger the collider, animation runs open the door
+        doorAnimation.SetBool("isOpening", true);
+    }
+
+    //Close the door
+    void onTriggerExit(Collider other)
+    {
+        doorAnimation.SetBool("isOpening", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
